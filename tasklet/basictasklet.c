d4239304245bd34234d7223efcf882badbf8e2bd
jjose1997 <jerinjose@mirafra.com>
2026-03-03 12:51:22 +0530

    Add files via upload

---
+++ tasklet/basictasklet.c
@@ -0,0 +1,52 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+
+#define KEYBOARD_IRQ 1
+
+static void keyboard_tasklet_fn(struct tasklet_struct *t);
+
+DECLARE_TASKLET(keyboard_tasklet, keyboard_tasklet_fn);
+
+static void keyboard_tasklet_fn(struct tasklet_struct *t)
+{
+	pr_info("tasklet: bottom half executed\n");
+}
+
+//top half IRQ handler
+static irqreturn_t keyboard_irq_handler(int irq, void *dev_id)
+{
+	pr_info("irq: keyboard interrupt occurred\n");
+	
+	tasklet_schedule(&keyboard_tasklet);
+	
+	return IRQ_HANDLED;
+}
+
+static int __init tasklet_irq_init(void)
+{
+	int ret;
+	ret=request_irq(KEYBOARD_IRQ,keyboard_irq_handler, IRQF_SHARED,"tasklet",(void *)keyboard_irq_handler);
+
+	if(ret)
+	{
+		pr_err("failed to request IRQ: %d\n",KEYBOARD_IRQ);
+		return ret;
+	}
+	pr_info("tasklet module loaded\n");
+	return 0;
+}
+
+static void __exit tasklet_irq_exit(void)
+{
+	tasklet_kill(&keyboard_tasklet);
+	free_irq(KEYBOARD_IRQ, (void *)keyboard_irq_handler);
+}
+
+module_init(tasklet_irq_init);
+module_exit(tasklet_irq_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jerin Jose");
+MODULE_DESCRIPTION("Simple tasklet code");
